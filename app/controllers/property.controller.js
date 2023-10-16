428608a379bd906733ffdfb8dfdc8a4c9440f2ee
lawal <lawalalaoad@gmail.com>
2023-10-16 12:18:47 +0200

    setting up crud for property and route access

---
+++ app/controllers/property.controller.js
@@ -0,0 +1,79 @@
+const Property = require('../models/property.model');
+exports.createProperty = async (req, res) => {
+    try {
+        const userRole = req.userRole; 
+
+        let ownerId;
+
+        if (userRole === 'AGENCY_MEMBER') {
+            ownerId = req.body.owner_id;
+            if (!ownerId) {
+                return res.status(400).send({ message: 'Owner ID is required for agency members.' });
+            }
+        } else if (userRole === 'OWNER') {
+            ownerId = req.userId;
+        } else {
+            return res.status(403).send({ message: 'Unauthorized.' });
+        }
+
+        const property = new Property({
+            name: req.body.name,
+            address: req.body.address,
+            type: req.body.type,
+            owner_id: ownerId
+        });
+
+        await property.save();
+
+        res.status(201).send({ message: 'Property created successfully!', property });
+    } catch (error) {
+        console.error("Error creating property:", error.message);
+        res.status(500).send({ message: 'Failed to create property', details: error.message });
+    }
+};
+
+
+exports.getAllProperties = async (req, res) => {
+    try {
+        const properties = await Property.find();
+        res.status(200).send(properties);
+    } catch (error) {
+        res.status(500).send({ message: 'Failed to fetch properties', details: error.message });
+    }
+};
+
+exports.getPropertyById = async (req, res) => {
+    try {
+        const property = await Property.findById(req.params.id);
+        if (!property) {
+            return res.status(404).send({ message: 'Property not found' });
+        }
+        res.status(200).send(property);
+    } catch (error) {
+        res.status(500).send({ message: 'Failed to fetch property', details: error.message });
+    }
+};
+
+exports.updateProperty = async (req, res) => {
+    try {
+        const property = await Property.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!property) {
+            return res.status(404).send({ message: 'Property not found' });
+        }
+        res.status(200).send({ message: 'Property updated successfully!', property });
+    } catch (error) {
+        res.status(500).send({ message: 'Failed to update property', details: error.message });
+    }
+};
+
+exports.deleteProperty = async (req, res) => {
+    try {
+        const property = await Property.findByIdAndDelete(req.params.id);
+        if (!property) {
+            return res.status(404).send({ message: 'Property not found' });
+        }
+        res.status(200).send({ message: 'Property deleted successfully!' });
+    } catch (error) {
+        res.status(500).send({ message: 'Failed to delete property', details: error.message });
+    }
+};
