288f95bb80cbb7ea30d5a02c5ef8b33dce44e96b
lawal <lawalalaoad@gmail.com>
2023-10-16 17:14:17 +0200

    setting to unique agency member email

--- app/models/agencyMember.model.js
+++ app/models/agencyMember.model.js
@@ -13,7 +13,8 @@ const AgencyMember = mongoose.model(
     },
     email: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     },
     password: {
       type: String,
